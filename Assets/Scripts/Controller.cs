e95b582d17f73da759c99d7ca8929ce588ed7f3d
Gabriel_dcl_Fonseca <gabrieldclf@gmail.com>
2025-10-04 22:15:43 -0300

    Update 07 - Coding
    Começando a colocar o código com base nas aulas de murilo e a atividade de Luna, testar também

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -3,24 +3,90 @@ using UnityEngine;
 [RequireComponent(typeof(Animator), typeof(Rigidbody2D), typeof(SpriteRenderer))]
 public class Controller : MonoBehaviour
 {
+    [Header("Configurações de Movimento")]
+    [SerializeField] float velocidadeX_run = 3.5f;
+    [SerializeField] float velocidadeY_jump = 7f;
 
-    [Header("Configurações")]
+
+    [Header("Verificar Chão")]
+    [SerializeField] float raycastDistance = 0.7f;
+    [SerializeField] LayerMask groundCheck;
+
+    [Header("Verificar Parede")]
+    [SerializeField] Transform checkWall;
+    [SerializeField] float wallDistanceCheck = 0.4f;
+    [SerializeField] LayerMask wallClimbCheck;
 
     //Variaveis, ver se preciso definir os boleanos das transições
     private Animator animator;
     private Rigidbody2D physics;
-    private bool isGrounded;
-    private bool isNearWall;
+    private bool isGrounded, airborne, isNearWall, doubleJump = true; //checar se está no chão, ar, proximo à parede, ou se ja deu um pulo duplo
     private float defaultGravityScale;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
 
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        physics = GetComponent<Rigidbody2D>();
+        defaultGravityScale = physics.gravityScale;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        PlayerInput(); //controlar comandos
+    }
 
+    void FixedUpdate()
+    {
+        Movimentacao(); //lidar com movimentação
+        UpdateAnimatorInfo(); //atualizar Parameters do animator
+    }
+    private void PlayerInput()
+    {
+        /*
+        * Comandos:
+            Barra de Espaço = Pular
+            Ataque com espada = Z
+            Disparo com pistola = X
+            Arremesso = C
+            Deslizar = V
+        */
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            animator.SetTrigger("jump");
+            physics.linearVelocity = new Vector2(physics.linearVelocity.x, velocidadeY_jump);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            animator.SetTrigger("slash");
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            animator.SetTrigger("shoot");
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            animator.SetTrigger("throw");
+        }
+
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            animator.SetTrigger("slide");
+        }
+    }
+
+    private void Movimentacao(){
+        
+    }
+
+    private void UpdateAnimatorInfo()
+    {
+        animator.SetFloat("velocidadeX", Mathf.Abs(physics.linearVelocity.x));
+        animator.SetFloat("velocidadeY", physics.linearVelocity.y);
+        animator.SetBool("estaNoChao", isGrounded);
+        animator.SetBool("estaNoAr", airborne);
+        animator.SetBool("doubleJump", doubleJump);
     }
 }
